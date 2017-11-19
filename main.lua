9b938c4a3cf01782749e3081d660062915feab72
ItsPax <alexsieu14@gmail.com>
2017-11-19 04:12:14 -0700

    more more more!

--- main.lua
+++ main.lua
@@ -1,19 +1,22 @@
 --TODO:
---  Implement goodies to incentivize the player
---  Implement fastfall NEEDS TO BE DONE
 --  Implement a diffculty curve
 --  Pseudorandomize, try to make it a little harder
+--      - can you write that to a file then pull it up every time?
+--  Implement double jump ( a little)
 
 --player and general game
+--
+--DIFFICULTY IMPLEMENTATION
+--  You're given only 30 seconds of time to begin with, but if you touch a yellow block then you gain more time
 player = {}
 platforms = {}
-player.speed = 200
 platformTimer = 0.01
 maxPlatformTimer = 1.0
 numPlatforms = 0
 gameOver = false
 yMove = 0
 score = 0
+highScore = score
 
 -- enemy
 createEnemyTimerMax = 1
@@ -23,10 +26,19 @@ enemies = {}
 
 --projectile
 canShoot = true
-canShootTimerMax = 0.5
+canShootTimerMax = 0.4
 canShootTimer = canShootTimerMax
 bulletImg = nil
 bullets = {}
+
+--coins
+createCoinTimerMax = 3
+createCoinTimer = createCoinTimerMax
+coinImg = nil
+coins = {}
+
+-- life remaining
+remainingHealthTimer = 30
 
 function love.load()
     player.x = 100 
@@ -36,14 +48,15 @@ function love.load()
     player.yVelocity = 0
     player.gravity = -500
     player.ground = love.graphics.getHeight()
-    player.jumpHeight = -600
+    player.jumpHeight = -400
 
 
     -- load images
     player.img = love.graphics.newImage('purple.png')
     platformImg = love.graphics.newImage('teal.png')
     enemyImg = love.graphics.newImage('red.png')
-    bulletImg = love.graphics.newImage('white.png')
+    bulletImg = love.graphics.newImage('black.png')
+    coinImg = love.graphics.newImage('orange.png')
 end
 
 function love.update(dt)
@@ -53,17 +66,21 @@ function love.update(dt)
 
         -- construct the platforms
         if platformTimer < 0 then
-            newPlatform = { x = 1040, y = 700, img = platformImg }
+            newPlatform = { x = 600, y = 700, img = platformImg }
             table.insert(platforms, newPlatform)
             numPlatforms = numPlatforms + 1
             platformTimer = maxPlatformTimer
         end
         platformTimer = platformTimer - dt
         if love.keyboard.isDown('s') then
-            platforms[numPlatforms].y = platforms[numPlatforms].y + dt*350
+            if numPlatforms > 0 then
+            platforms[numPlatforms].y = platforms[numPlatforms].y + dt*400
+            end
         end
         if love.keyboard.isDown('w') then 
-            platforms[numPlatforms].y = platforms[numPlatforms].y - dt*350
+            if numPlatforms > 0 then
+            platforms[numPlatforms].y = platforms[numPlatforms].y - dt*400
+            end
         end
         for i, platform in ipairs(platforms) do
             platform.x = platform.x - dt*180
@@ -71,12 +88,20 @@ function love.update(dt)
         end
         
         -- implementing fastfall
-        
+        if love.keyboard.isDown('down') then
+            player.yVelocity = player.yVelocity + 200*dt
+        end
+            
 
         --handle jumping of the player
         if love.keyboard.isDown('up') then 
-            if player.yVelocity == 0 or math.abs(player.y - platforms[numPlatforms].y) < 10 then
-                player.yVelocity = player.jumpHeight
+            if player.yVelocity ~= 0 then
+                player.yVelocity = player.yVelocity - 200*dt
+            end
+            if numPlatforms > 0 then
+                if player.yVelocity == 0 or math.abs(player.y - platforms[numPlatforms].y) < 10 then
+                    player.yVelocity = player.jumpHeight
+                end
             end
         end
         
@@ -116,11 +141,23 @@ function love.update(dt)
 
         -- update positions of enemies
         for i, enemy in ipairs(enemies) do
-            enemy.x = enemy.x - (200*dt)
+            enemy.x = enemy.x - (300*dt)
             if enemy.x < 0 then
                 table.remove(enemies,i)
             end
         end
+
+        --implement coins
+        if createCoinTimer < 0 then
+            randomNumber = math.random(10, love.graphics.getHeight() - 10)
+            newCoin = {x = 1280, y = randomNumber, img = coinImg}
+            table.insert(coins, newCoin)
+            createCoinTimer = createCoinTimerMax
+            remainingHealthTimer = remainingHealthTimer + 10
+
+        end
+        createCoinTimer = createCoinTimer - dt
+
         
         -- handle shooting
         canShootTimer = canShootTimer - dt
@@ -143,6 +180,14 @@ function love.update(dt)
             end
         end
 
+        --update the positions of the coins
+        for i,coin in ipairs(coins) do
+            coin.x = coin.x - 250*dt
+            if coin.x < 0 then
+                table.remove(coins,i)
+            end
+        end
+
         -- handle collision of bullets and enemies
         for i,bullet in ipairs(bullets) do 
             for j,enemy in ipairs(enemies) do
@@ -162,6 +207,31 @@ function love.update(dt)
             end
         end
 
+        --handle collision of coins and player
+        for i,coin in ipairs(coins) do
+            if checkCollisionBullet(player.x,player.y,player.img:getWidth(),player.img:getHeight(), coin.x,coin.y, coin.img:getWidth(), coin.img:getHeight()) then
+                score = score + 10
+                table.remove(coins,i)
+            end
+        end
+
+        -- tick the remaining health
+        remainingHealthTimer = remainingHealthTimer - dt
+
+    else --what if it's gameover?
+        print("restarting...")
+        numPlatforms = 0
+        if score > highScore then
+            highScore = score
+        end
+        score = 0
+        platforms = {}
+        coins = {}
+        bullets = {}
+        enemies = {}
+        gameOver = false
+        love.timer.sleep(1)
+        love.load()
     end
 end
 
@@ -199,8 +269,9 @@ end
 
 function love.draw()
     -- handle gameover  case
+    love.graphics.setBackgroundColor(112,128,144)
     if gameOver then
-        love.graphics.print("GAME OVER!", 50, 50)
+        love.graphics.print("GAME OVER!", love.graphics.getWidth()/2, love.graphics.getHeight()/2,0,5,5)
     end
     love.graphics.draw(player.img,player.x,player.y, 0, 1, 1, 0, 32) -- first 0 is angle
     for i, platform in ipairs(platforms) do
@@ -214,5 +285,10 @@ function love.draw()
         love.graphics.draw(bullet.img,bullet.x,bullet.y)
     end
 
-    love.graphics.print(score, 50, 100)
+    for i, coin in ipairs(coins) do
+        love.graphics.draw(coin.img,coin.x,coin.y)
+    end
+    love.graphics.print("Score:"..score, 50, 100, 0, 3, 3)
+    love.graphics.print("High Score:"..highScore, 50,150,0,3,3)
+    love.graphics.print("Life Remaining: "..math.floor(remainingHealthTimer), 50, 200, 0, 3,3)
 end
